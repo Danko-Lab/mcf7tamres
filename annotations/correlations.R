61bb23f548960e9aad39056cbb221e2052dbe905
Charles <dankoc@gmail.com>
2015-06-17 11:43:51 -0400

    Making sure to print a pdf in annotations_correaltions_R

--- annotations/correlations.R
+++ annotations/correlations.R
@@ -90,10 +90,12 @@ write.csv(cbind(refGene, gene_body_counts), "gene_body_counts.csv")
 
 ## Create a colorscatterplot
 source("../lib/densScatterplot.R")
+pdf("DensityScatterplots.pdf")
 indx <- (B7c > 0 & B7e > 0 & B7eb > 0 & B7 > 0 & B7b > 0)
 densScatterplot(B7[indx], B7b[indx], uselog=TRUE, xlab="B7", ylab="B7+BBCA")
 densScatterplot(B7c[indx], B7e[indx], uselog=TRUE, xlab="B7 -- Control", ylab="B7 + E2")
 densScatterplot(B7c[indx], B7eb[indx], uselog=TRUE, xlab="B7 -- Control", ylab="B7 + E2 + BBCA")
+dev.off()
 
 ## Cluster changed genes...
 yb.sig.pal <- function(n, scale=10) {
